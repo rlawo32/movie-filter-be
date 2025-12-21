b0e7248c0534f045174b098e47ef205e8a1ff1ae
rlawo <rlawo32@naver.com>
2025-12-21 19:37:40 +0900

    First Commit

---
+++ src/main/java/com/project/moviefilterbe/MovieFilterBeApplication.java
@@ -0,0 +1,13 @@
+package com.project.moviefilterbe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MovieFilterBeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MovieFilterBeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/project/moviefilterbe/MovieFilterBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.moviefilterbe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MovieFilterBeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
